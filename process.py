1facceb4e85b426bed102ece99eed3cfc4d6d1e3
Davy Leggieri <davy.leggieri@razerzone.com>
2017-02-26 18:24:10 -0800

    Removed some comments from process.py

--- process.py
+++ process.py
@@ -101,14 +101,11 @@ def find_cars(img, y_start, y_stop, scale, clf, X_scaler, color_space='RGB',
 
 
 def add_heat(heatmap, bbox_list):
-    # Iterate through list of bboxes
     for box in bbox_list:
         # Add += 1 for all pixels inside each bbox
         # Assuming each "box" takes the form ((x1, y1), (x2, y2))
         heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
-
-    # Return updated heatmap
-    return heatmap  # Iterate through list of bboxes
+    return heatmap
 
 
 def apply_threshold(heatmap, threshold):
@@ -119,7 +116,6 @@ def apply_threshold(heatmap, threshold):
 
 
 def draw_labeled_bboxes(img, labels):
-    # Iterate through all detected cars
     for car_number in range(1, labels[1] + 1):
         # Find pixels with each car_number label value
         nonzero = (labels[0] == car_number).nonzero()
@@ -130,7 +126,6 @@ def draw_labeled_bboxes(img, labels):
         bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
         # Draw the box on the image
         cv2.rectangle(img, bbox[0], bbox[1], (0, 0, 255), 6)
-    # Return the image
     return img
 
 
